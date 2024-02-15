785826096ca5eeaab7960fa43c2e3ffb5e66dbe2
Shubham Kaura <shubhamkaura8@gmail.com>
2024-02-15 01:43:05 -0700

    Add card data and scroll down animations classes

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,9 +1,36 @@
-const title = document.querySelector(".main-body__title");
+const title = document.querySelector(".main-title");
+// const cardsList = document.querySelectorAll(".card");
+
+// let animationIteration = 1;
+// let curCard = 0;
+
+// window.addEventListener("wheel", (e) => {
+//   if (e.deltaY === 100) {
+//     if (curCard > cardsList.length - 1) {
+//       return;
+//     }
+//     console.log(curCard);
+//     console.log(cardsList.length - 1);
+//     let card = cardsList[curCard];
+//     if (curCard > 0) {
+//       cardsList[curCard - 1].classList.add("slide-out-bck-top");
+//       cardsList[curCard - 1].classList.remove("slide-in-bck-bottom");
+//     }
+//     card.classList.remove("hidden");
+//     card.classList.add("slide-in-bck-bottom");
+//     curCard += 1;
+//   }
+// });
 
 window.addEventListener("load", () => {
-  title.textContent = "I'm a";
+  title.textContent = "HELLO!";
   title.classList.add("tracking-in-expand");
 });
+
+const changeAnimationClass = function () {
+  title.classList.remove("tracking-out-contract");
+  title.classList.add("tracking-in-expand");
+};
 
 title.addEventListener("animationend", () => {
   if (title.classList.contains("tracking-in-expand")) {
@@ -12,11 +39,23 @@ title.addEventListener("animationend", () => {
     return;
   }
   if (title.classList.contains("tracking-out-contract")) {
-    title.textContent === "I'm a"
-      ? (title.textContent = "Web Developer!")
-      : (title.textContent = "I'm a");
-    title.classList.remove("tracking-out-contract");
-    title.classList.add("tracking-in-expand");
-    return;
+    if (animationIteration === 1) {
+      title.textContent = "I'm a,";
+      animationIteration = 2;
+      changeAnimationClass();
+      return;
+    }
+    if (animationIteration === 2) {
+      title.textContent = "WEB DEVELOPER!";
+      animationIteration = 3;
+      changeAnimationClass();
+      return;
+    }
+    if (animationIteration === 3) {
+      title.textContent = "HELLO!";
+      animationIteration = 1;
+      changeAnimationClass();
+      return;
+    }
   }
 });
